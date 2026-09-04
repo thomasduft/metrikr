de95adca7a9ac40f8a147cccbdbc63f3e96b2e8b
Thomas Duft <thomas.duft@abraxas.ch>
2026-09-04 08:37:33 +0200

    Upgrade .NET SDK version to 10.0, update Fluid.Core package to 2.40.0, and enhance install script for better version handling

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
+TOOLNAME="tomware.MetrikR"
+echo $TOOLNAME
+
 if [ -z "$1" ];
 then
+  echo ----
   echo No version specified! Please specify a valid version like 1.2.3 or 1.2.3-rc1!
-  exit 1
-fi
-
-if [ -z "$2" ];
-then
-  echo No release info specified! For a relase provide the r flag and a valid release verion!
   exit 1
 fi
 
@@ -17,33 +15,20 @@ echo Starting building version $1
 
 echo ----
 echo Cleaning up
-rm -r ./dist
-dotnet tool uninstall -g tomware.MetrikR
+rm -rf ./artifacts
+dotnet tool uninstall -g $TOOLNAME
 
 echo ----
-echo Restore solution
+echo Restore
 dotnet restore src/metrikr
 
 echo ----
-if [ $2 = "i" ];
-then
-  echo Packaging solution with Version = $1
-  dotnet pack src/metrikr -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
-else
-  echo Packaging solution with PackageVersion = $1
-  dotnet pack src/metrikr -c Release -p:PackageVersion=$1 -o ./dist/nupkgs/
-fi
+echo Packaging with Version = $1
+dotnet pack src/metrikr -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./artifacts/nupkgs/
 
 echo ----
-if [ -z "$3" ];
-then
-  echo No publish info specified! For publishing metrikr provide the p flag!
-  exit 1
-fi
-
-echo ----
-echo Installing metrikr globally with Version = $1
-dotnet tool install --global --add-source ./dist/nupkgs/ tomware.MetrikR
+echo Installing globally with Version = $1
+dotnet tool install --global --add-source ./artifacts/nupkgs/ $TOOLNAME
 
 echo ----
 echo Done
